b1540e716e798bb2dfd33eb26ae27818b747d6a6
Евгений <evgeniytem2007@gmail.com>
2025-06-20 21:39:18 +1000

    Приложение Pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
+from tkinter.font import names
+
 from django.urls import path
-from .views import home_page_view
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path("", home_page_view, name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
-from django.http import HttpResponse
-def home_page_view(request):
-    return HttpResponse("Hello, Django!")
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
